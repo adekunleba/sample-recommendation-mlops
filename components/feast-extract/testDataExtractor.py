d64a1c5879e4e528553741f895b3f0de58ffe256
Babatunde <adekunleba@gmail.com>
2021-08-09 17:21:45 +0100

    initial feast extract

---
+++ components/feast-extract/testDataExtractor.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestDataExtractor(unittest.TestCase):
+
+    def retrieve_feast(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == "__main__":
+    unittest.main()
